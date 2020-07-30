1d16362c024e6ff6fd331f0a8eb747f4f4566f3a
mattzamora21 <68615568+mattzamora21@users.noreply.github.com>
2020-07-29 23:49:40 -0700

    Create Assignment1.java
    I just uploaded my assignment 1 that I worked on in my previous CS classes.

---
+++ Assignment1.java
@@ -0,0 +1,32 @@
+// Assignment #: 1
+//         Name: MATTHEW ZAMORA
+//   Student ID: 1212882006
+//      Lecture: TUES/THU 4:30PM
+//  Description: This class reads an integer from a keyboard and prints it out along with other messages.
+
+
+
+import java.util.Scanner; // use the Scanner class located in the "java.util" directory 
+
+
+
+public class Assignment1 
+ {
+  public static void main (String[] args)   
+   {    
+	 int number;   
+			
+	 Scanner console = new Scanner(System.in);
+	 
+	 number = console.nextInt();     // read an integer entered by a user 
+	 
+	 // display the number with other messages 
+	 	   System.out.print("This program reads an integer from a keyboard,\n"
+			              + "and prints it out on the display screen.\n"
+			              + "Make sure that you get the exact same output as the expected one.\n"
+			              + "The read number is " + number + ".\n");
+			 
+		}
+	}
+
+
